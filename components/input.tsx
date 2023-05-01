f9f1ef3ed8b09597a82179b676b5a64b59a928c9
Kendall McGree <mcgreekendall@gmail.com>
2023-05-01 17:35:45 -0500

    add type definition for props that come into Input component from Auth. Add those props to the Input call.

--- components/input.tsx
+++ components/input.tsx
@@ -1,9 +1,21 @@
-const Input = () => {
+import React from "react";
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: string;
+}
+
+const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
     <>
       <div className="relative">
         <input
-          id="email"
+          value={value}
+          type={type}
+          onChange={onChange}
+          id={id}
           className="
         block
         rounded-md
@@ -39,9 +51,9 @@ const Input = () => {
         peer-focus:scale-75
         peer-focus:-translate-y-3
         "
-          htmlFor="email"
+          htmlFor={id}
         >
-          Email
+          {label}
         </label>
       </div>
     </>
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -12,7 +12,13 @@ const Auth = () => {
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
             <div className="flex flex-col gap-4">
-              <Input />
+              <Input
+                label="Email"
+                onChange={() => {}}
+                id="email"
+                type="email"
+                value=""
+              />
             </div>
           </div>
         </div>
