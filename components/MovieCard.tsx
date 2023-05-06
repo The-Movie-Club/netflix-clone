40d6733ac04f82b38956e0ce86b816214269cfdf
Kendall McGree <mcgreekendall@gmail.com>
2023-05-06 14:17:21 -0500

    add functionaloty to open modal to Billboard and MovieCard component, bug wont allow modal to close

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
 import router from "next/router";
+import useInfoModal from "@/hooks/useInfoModal";
+import { BiChevronDownCircle } from "react-icons/bi";
+
 interface MovieCardProps {
   data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const { openModal } = useInfoModal();
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -98,6 +102,26 @@ hover:bg-neutral-300
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item
+            w-6 h-6 
+            lg:w-10 
+            lg:h-10
+             border-white 
+             border-2 
+             rounded-full
+              flex
+               justify-center
+                items-center
+                 transition
+                  hover:border-neutral-300"
+            >
+              <BiChevronDownCircle
+                size={30}
+                className="text-white group-hover/item:text-neutral-300"
+              />
+            </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New{" "}
